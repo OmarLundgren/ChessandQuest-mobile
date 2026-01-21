5e863b43e0217111a0639511c3d6faa374a45ae4
theo <theo.wikstromm@outlook.com>
2026-01-21 14:23:07 +0100

    weburl

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, StatusBar } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hej!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        <WebView
+            source={{ uri: 'https://chessandquest2-fsheevcjccemcyd3.norwayeast-01.azurewebsites.net/Player/SignIn' }} // <-- byt till er webapp URL
+            style={{ flex: 1 }}
+            originWhitelist={['*']}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            startInLoadingState={true}
+        />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
